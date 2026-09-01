9d2cbff0bf08ad59f35fc1e4f40d4c3fa9420b09
Louis <louis.contact@icloud.com>
2026-09-01 21:30:17 +0200

    Add: jump-game - submission-5

---
+++ Data Structures & Algorithms/jump-game/submission-5.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        if (nums.size() == 1) return true;
+        if (nums[0] == 0) return false;
+
+        int maxre = 0;
+
+        for (int i = 0; i < nums.size() - 1; i++) {
+            maxre = max(maxre, i + nums[i]);
+            if (i + 1 > maxre) return false;
+            if (maxre >= nums.size() - 1) return true;
+        }
+
+        return false;
+    }
+};
