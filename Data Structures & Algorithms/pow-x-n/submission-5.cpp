4ffc5bef6a13b63a57cebf796c44ac8ce433d9af
Louis <louis.contact@icloud.com>
2026-09-09 10:47:42 +0200

    Add: pow-x-n - submission-5

---
+++ Data Structures & Algorithms/pow-x-n/submission-5.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+
+    double pow(double x, int n) {
+        if (n == 0){
+            return 1.0;
+        } 
+        else if (n % 2 == 0) {
+            return myPow(x * x, n / 2);
+        } 
+        return x * myPow(x * x, (n - 1) / 2);
+    }
+
+    double myPow(double x, int n) {
+        if (n < 0) {
+            x = 1.0/x;
+            n = -n;
+        }
+
+        return pow(x,n);
+    }
+};
